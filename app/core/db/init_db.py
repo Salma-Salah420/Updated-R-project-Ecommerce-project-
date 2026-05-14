11b175a7ce9db6a5d9b4d677197edd112851d94b
AhmedTarq0-0 <at1819527@gmail.com>
2026-05-14 08:07:28 +0300

    Delete app/core/db directory

--- app/core/db/init_db.py
+++
@@ -1,9 +0,0 @@
-
-from sqlalchemy.orm import Session
-
-from app.db.seed_service import SeedService
-
-
-def init_db(db: Session) -> dict:
-    service = SeedService(db)
-    return service.seed_all()
